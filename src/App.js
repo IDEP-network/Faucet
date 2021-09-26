20a596e6f7fa859ef3f5fd671d3b643c0689732b
aidasap <aidas.ap@gmail.com>
2021-09-26 19:15:12 -0400

    updated captcha key

--- src/App.js
+++ src/App.js
@@ -127,7 +127,7 @@ class App extends React.Component {
                   className="btn btn-info mt-3"
                 />
                    <Recaptcha
-            sitekey="6Le_SZAcAAAAANMWdmDJKQgoaF1QwXWZR6I9Ulxi"
+            sitekey="6LeLa4ocAAAAALHMRehSUtwG9Y1WN9eJaPd9SnpZ"
             render="explicit"
             onloadCallback={this.recaptchaLoaded}
             verifyCallback={this.verifyCallback}
