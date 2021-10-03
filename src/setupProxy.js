43477947d3d65477ded4e6039ff1613d6bb2cf35
Aidas <65505947+aidasap@users.noreply.github.com>
2021-10-03 11:22:12 -0400

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api1', {
-      target: 'http://64.225.75.108:8080/faucet', // API endpoint 1
+      target: 'https://api.idep.network:8080/faucet', // API endpoint 1
       changeOrigin: true,
       pathRewrite: {
         "^/api1": "",
@@ -15,7 +15,7 @@ module.exports = function (app) {
   );
   app.use(
     createProxyMiddleware('/api2', {
-      target: 'http://64.225.75.108:8080/createAcc', // API endpoint 2
+      target: 'https://api.idep.nework:8080/createAcc', // API endpoint 2
       changeOrigin: true,
       pathRewrite: {
         "^/api2": "",
